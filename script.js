e2d6cfce8060007ddec785d044c87514426944ac
Eduardo <ed.luna80@gmail.com>
2022-06-27 19:25:32 -0700

    Gameboard groundwork
    Add gameboard div with tiles for each tic-tac-toe space to index.html.
    Add CSS grid and basic styling to gameboard div. Create Gameboard module
    to assign x or o to each tile based on an array with each selection.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('hello world');
+
+const Gameboard = (() => {
+    let gameboard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    let tiles = document.getElementsByClassName('game-tile');
+    const assignSelections = () => {
+        for (i = 0; i < gameboard.length; i++) {
+            if (gameboard[i] === 'x') {
+                tiles[i].innerHTML = 'x'
+            }
+            else if (gameboard[i] === 'o') {
+                tiles[i].innerHTML = 'o'
+            }
+
+            else {
+                tiles[i].innerHTML = ' ';
+            }
+        }
+    }; 
+    return {
+        tiles,
+        assignSelections,
+    }
+
+})();
+
+Gameboard.assignSelections();
