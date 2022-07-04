f3edab03f62777cb301c13b1c6b2ea4fda8ebda2
Eduardo <ed.luna80@gmail.com>
2022-07-04 15:47:51 -0700

    Gameboard interactivity
    Create base logic for users to add x and os to gameboard. Need to write
    logic to check for winner/tie.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-console.log('hello world');
+const playerFactory = (name, side) => {
+    return {
+        name, side,
+    }
+};
 
+const playerX = playerFactory('Joe Schmoe', 'x');
+
+const playerY = playerFactory('John Doe', 'o');
+
+/* module to keep track of game info */ 
+const Game = (() => {
+    let turn = ' ';
+
+    return { 
+        turn, 
+    }
+
+})();
+
+Game.turn = 'x';
+
+/* Module for interacting with the html game board */ 
 const Gameboard = (() => {
-    let gameboard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    let gameboard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     let tiles = document.getElementsByClassName('game-tile');
+    
+    
     const assignSelections = () => {
         for (i = 0; i < gameboard.length; i++) {
             if (gameboard[i] === 'x') {
@@ -17,11 +40,47 @@ const Gameboard = (() => {
             }
         }
     }; 
+
+
+    const activateTiles = () => {
+        for (let i = 0; i < gameboard.length; i++) {
+            tiles[i].addEventListener('click', () => {
+                if (Game.turn === 'x') {
+                    gameboard[i] = 'x';
+                    assignSelections();
+                    Game.turn = 'o';
+                }
+                else if (Game.turn === 'o') {
+                    gameboard[i] = 'o';
+                    assignSelections();
+                    Game.turn = 'x';
+                }
+            })
+
+        }
+
+    }
+
     return {
+        gameboard,
         tiles,
         assignSelections,
+        activateTiles,
     }
 
 })();
 
-Gameboard.assignSelections();
+Gameboard.activateTiles();
+
+
+
+
+
+const Display = (() => {
+    let turnIndicator = document.getElementById('control-display');
+
+    return {
+        turnIndicator,
+    }
+
+})();
